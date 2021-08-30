70e5229f783ad9d327fd19be46a5d831cc1a801c
涂瀚滨 <malakathb@163.com>
2021-08-30 22:42:58 +0800

    zk link commit03

---
+++ 008-zk-consumer/src/main/java/com/thbpowernode/dubbo/web/UserController.java
@@ -0,0 +1,24 @@
+package com.thbpowernode.dubbo.web;
+
+import com.thbpowernode.dubbo.model.User;
+import com.thbpowernode.dubbo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/userDetail")
+    public String userDetail(Model model,Integer id, String username){
+        User user = userService.queryUserById(id,username);
+        model.addAttribute(user);
+        return "userDetail";
+    }
+}
---
+++ 008-zk-consumer/src/main/webapp/userDetail.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: THB
+  Date: 2021/8/30
+  Time: 22:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>用户信息</title>
+</head>
+<body>
+<h1>用户信息</h1>
+<div>用户编号：${user.id}</div>
+<div>用户姓名：${user.username}</div>
+</body>
+</html>
